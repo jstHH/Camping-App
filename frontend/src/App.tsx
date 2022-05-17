c5e71ae1830f18bf76e5cf356a4ae720198ef801
Joachim <joachim.stolle@hotmail.de>
2022-05-17 17:27:12 +0200

    removed favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Testheader</h1>
+        <h1>Testheader Neu</h1>
       </header>
     </div>
   );
