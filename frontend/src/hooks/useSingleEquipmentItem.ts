04494028c17cb6e3269edcbb9b1919d9d3e10d3a
Joachim <joachim.stolle@hotmail.de>
2022-05-25 16:52:37 +0200

    added api request and hook

---
+++ frontend/src/hooks/useSingleEquipmentItem.ts
@@ -0,0 +1,18 @@
+import {useContext, useState} from "react";
+import {EquipmentItem} from "../model/EquipmentItem";
+import {AuthContext} from "../context/AuthProvider";
+import {getEquipmentItemByID} from "../service/EquipmenItemApiService";
+
+
+export default function useSingleEquipmentItem() {
+    const [equipmentItem, setEquipmentItem] = useState<EquipmentItem>()
+    const {token} = useContext(AuthContext)
+
+    const getSingleEquipmentItemByID = (id: string) => {
+        getEquipmentItemByID(id,token)
+            .then(response => setEquipmentItem(response))
+            .catch(console.error)
+    }
+
+    return {equipmentItem, getSingleEquipmentItemByID}
+}
