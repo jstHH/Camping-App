c076b9f8b65ffd297f9364ebc9e8774ccd761a17
Joachim <joachim.stolle@hotmail.de>
2022-06-01 15:12:25 +0200

    Added Page components

---
+++ frontend/src/components/CarTopView.tsx
@@ -0,0 +1,10 @@
+import {Button} from "react-bootstrap";
+
+
+export default function CarTopView() {
+
+    return <div>
+        <h2>Autos</h2>
+        <Button variant={"primary"} type={"button"}>Neues Auto</Button>
+    </div>
+}
--- frontend/src/pages/CampsitePage.tsx
+++ frontend/src/pages/CampsitePage.tsx
@@ -1,6 +1,7 @@
 import AppHeader from "../components/AppHeader";
 import {CarItem} from "../model/CarItem";
 import {AppUser} from "../model/AppUser";
+import CarOverview from "../components/CarOverview";
 
 export type CampsitePageProps = {
     carItems: CarItem[]
@@ -10,5 +11,6 @@ export type CampsitePageProps = {
 export default function CampsitePage({carItems, appUsers} : CampsitePageProps) {
     return <div>
         <AppHeader/>
+        <CarOverview carItems={carItems} appUsers={appUsers} />
     </div>
 }
