28b42087ff2ee1998e215e4b60a0b121c98b1118
Joachim <joachim.stolle@hotmail.de>
2022-06-06 15:32:53 +0200

    added frontend components

---
+++ frontend/src/components/DetailsPageTextForm.tsx
@@ -0,0 +1,34 @@
+import {Button, Form} from "react-bootstrap";
+
+
+type DetailsPageTextFormProps = {
+    title:string
+    setTitle: (text:string) => void
+    description: string
+    setDescription: (text:string) => void
+    startlocation?: string
+    setStartLocation?: (text:string) => void
+    forCar: boolean
+    trailer: boolean
+}
+
+export default function DetailsPageTextForm({title, setTitle, description, setDescription, startlocation, setStartLocation, forCar, trailer}: DetailsPageTextFormProps) {
+    return <div>
+        <Form>
+            <Form.Group>
+                <Form.Label>Bezeichnung</Form.Label>
+                <Form.Control type="text" value={title} onChange={event => setTitle(event.target.value)}/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Anmerkungen</Form.Label>
+                {trailer && <Button variant={"outline-info"}>Anhänger</Button>}
+                <Form.Control as={"textarea"} rows={5} value={description} onChange={(event) => setDescription(event.target.value)}/>
+            </Form.Group>
+            {forCar && setStartLocation && <Form.Group>
+                <Form.Label>Startet Wo?</Form.Label>
+                <Form.Control type="text" value={startlocation} onChange={event => setStartLocation(event.target.value)}/>
+            </Form.Group>}
+        </Form>
+
+    </div>
+}
