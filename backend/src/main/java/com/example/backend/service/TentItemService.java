5a9319ad836a8b9af4832959d5bda0b34e7f1b54
Joachim <joachim.stolle@hotmail.de>
2022-06-07 18:56:59 +0200

    added post endpoint added tests

--- backend/src/main/java/com/example/backend/service/TentItemService.java
+++ backend/src/main/java/com/example/backend/service/TentItemService.java
@@ -1,6 +1,7 @@
 package com.example.backend.service;
 
 
+import com.example.backend.dto.TentItemDTO;
 import com.example.backend.model.TentItem;
 import com.example.backend.repository.TentItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,17 @@ public class TentItemService {
     public List<TentItem> getTentItems () {
         return tentItemRepository.findAll();
     }
+
+    public TentItem addTentItem (TentItemDTO newTentItem) {
+        return tentItemRepository.insert(TentItem.builder()
+                .title(newTentItem.getTitle())
+                .description(newTentItem.getDescription())
+                .owner(newTentItem.getOwner())
+                .involved(newTentItem.getInvolved())
+                .spending(newTentItem.getSpending())
+                .capacity(newTentItem.getCapacity())
+                .shelter(newTentItem.isShelter())
+                .build());
+    }
+
 }
