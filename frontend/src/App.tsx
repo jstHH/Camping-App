5fcdaca6cd931d01b9f67c8e0494943ed884a336
Joachim <joachim.stolle@hotmail.de>
2022-06-08 12:25:31 +0200

    added request and hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
     const currentUser = useAppUser()
     const appUsers = useAllAppUsers()
     const {carItems, addCarItem, updateCarItem, removeCarItem} = useCarItems()
-    const {tentItems, addTentItem} = useTentItems()
+    const {tentItems, addTentItem, updateTentItem} = useTentItems()
 
     return (
     <div className="App">
