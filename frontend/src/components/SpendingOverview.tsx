8ce72aa895e85e4c6bec8bddd3e8c835519effb9
Joachim <joachim.stolle@hotmail.de>
2022-06-09 20:53:35 +0200

    added balance overview to spending page

--- frontend/src/components/SpendingOverview.tsx
+++ frontend/src/components/SpendingOverview.tsx
@@ -2,6 +2,8 @@ import {Spending} from "../model/Spending";
 import {AppUser} from "../model/AppUser";
 import SpendingTopView from "./SpendingTopView";
 import SpendingCardView from "./SpendingCardView";
+import UserBalanceOverview from "./UserBalanceOverview";
+import "./SpendingOverview.css"
 
 
 export type SpendingOverviewProps = {
@@ -10,8 +12,13 @@ export type SpendingOverviewProps = {
 }
 
 export default function SpendingOverview({spendings, appUsers}: SpendingOverviewProps) {
-    return <div>
-        <SpendingTopView/>
-        <SpendingCardView spendings={spendings} appUsers={appUsers} />
+    return <div className={"overview"}>
+        <div className={"spending"}>
+            <SpendingTopView/>
+            <SpendingCardView spendings={spendings} appUsers={appUsers}/>
+        </div>
+        <div className={"balance"}>
+            <UserBalanceOverview appUsers={appUsers}/>
+        </div>
     </div>
 }
